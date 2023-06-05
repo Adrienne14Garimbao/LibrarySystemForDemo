a5d3693ec27014b0ab7d6967bb06a6457eae79b3
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-06-05 09:59:56 +0800

    Added AppServices for Departments

---
+++ src/LibrarySystemForDemo.Application/Departments/IDepartmentAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using LibrarySystemForDemo.Departments.Dto;
+using LibrarySystemForDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemForDemo.Departments
+{
+    public interface IDepartmentAppService : IAsyncCrudAppService<DepartmentDto, int, PagedDepartmentResultRequestDto, CreateDepartmentDto, DepartmentDto>
+    {
+
+    }
+}
