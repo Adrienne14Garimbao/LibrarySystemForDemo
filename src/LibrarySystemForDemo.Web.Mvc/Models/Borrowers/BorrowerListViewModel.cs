fd32a0123efa0a380462b034c5815b27e5154a64
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-06-05 16:29:58 +0800

    Added ListViewModel for Borrower

---
+++ src/LibrarySystemForDemo.Web.Mvc/Models/Borrowers/BorrowerListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using LibrarySystemForDemo.Borrowers.Dto;
+using System.Collections.Generic;
+
+namespace LibrarySystemForDemo.Web.Models.Borrowers
+{
+    public class BorrowerListViewModel
+    {
+
+        public List<BorrowerDto> Borrowers { get; set; }
+    }
+}
---
+++ src/LibrarySystemForDemo.Web.Mvc/Models/Borrowers/CreateOrEditBorrowerListViewModel.cs
@@ -0,0 +1,32 @@
+﻿using LibrarySystemForDemo.Books.Dto;
+using LibrarySystemForDemo.Entities;
+using LibrarySystemForDemo.Students.Dto;
+using System;
+using System.Collections.Generic;
+
+namespace LibrarySystemForDemo.Web.Models.Borrowers
+{
+    public class CreateOrEditBorrowerListViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime BorrowDate { get; set; }
+
+        public DateTime ExpectedReturnDate { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+
+        public int BookId { get; set; }
+
+        public Book Books { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public List<BookDto> BookList { get; set; }
+
+        public List<StudentDto> StudentList { get; set; }
+
+    }
+}
