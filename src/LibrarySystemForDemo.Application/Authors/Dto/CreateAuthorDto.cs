a9042df673f5382e63f0b144c7c107daa4043b44
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-06-05 10:26:38 +0800

    Added AppService for Authors

---
+++ src/LibrarySystemForDemo.Application/Authors/Dto/CreateAuthorDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using LibrarySystemForDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemForDemo.Authors.Dto
+{
+
+    [AutoMapTo(typeof(Author))]
+    [AutoMapFrom(typeof(Author))]
+    public class CreateAuthorDto : EntityDto<int>
+    {
+        public string AuthorsName { get; set; }
+
+    }
+}
---
+++ src/LibrarySystemForDemo.Application/Authors/IAuthorAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using LibrarySystemForDemo.Authors.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemForDemo.Authors
+{
+    public interface IAuthorAppService : IAsyncCrudAppService<AuthorDto, int, PagedAuthorResultRequestDto, CreateAuthorDto, AuthorDto>
+    {
+
+
+    }
+}
