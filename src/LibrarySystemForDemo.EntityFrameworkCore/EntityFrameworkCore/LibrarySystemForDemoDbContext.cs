ad36bbdd92e9bb759029e67d36aa4c102ef0881c
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-06-05 09:32:55 +0800

    Added Migration for Departments

--- src/LibrarySystemForDemo.EntityFrameworkCore/EntityFrameworkCore/LibrarySystemForDemoDbContext.cs
+++ src/LibrarySystemForDemo.EntityFrameworkCore/EntityFrameworkCore/LibrarySystemForDemoDbContext.cs
@@ -3,6 +3,7 @@ using Abp.Zero.EntityFrameworkCore;
 using LibrarySystemForDemo.Authorization.Roles;
 using LibrarySystemForDemo.Authorization.Users;
 using LibrarySystemForDemo.MultiTenancy;
+using LibrarySystemForDemo.Entities;
 
 namespace LibrarySystemForDemo.EntityFrameworkCore
 {
@@ -14,5 +15,24 @@ namespace LibrarySystemForDemo.EntityFrameworkCore
             : base(options)
         {
         }
+
+        /* Note:  
+        * 
+        * Don't forget to run 'add-migration' Then > 'update-database' 
+        * In Package Console Manager
+        * 
+        * and 
+        * 
+             using LibrarySystemForDemo.Entities;
+            : FullAuditedEntity<int>  
+        * 
+        */
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+
+
     }
 }
